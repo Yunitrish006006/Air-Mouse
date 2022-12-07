dc7e1c7cb02beabe7ea491b8ef724427c79a817a
Yunitrish <yunthomas006@gmail.com>
2022-12-07 14:56:14 +0800

    update

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
 import keyboard
 import pyautogui
+import pygetwindow
 
 print(pyautogui.size())
 pyautogui.moveTo(0, 0, duration=0.001)
 pyautogui.FAILSAFE = False
 keep = True
+screen_width = pyautogui.size()[0]
+screen_height = pyautogui.size()[1]
+x = pyautogui.position()[0]
+y = pyautogui.position()[1]
+vx = 32
+vy = 32
 while keep:
     if(keyboard.is_pressed('Esc')): keep = False
-    # pyautogui.moveTo(1920/2, 1080/2)
-    pyautogui.alert(text='', title='', button='OK')
+    # pyautogui.displayMousePosition()
+    pyautogui.moveTo(x,y)
+
+    x += vx
+    y += vy
+   
+    if(x >= screen_width or x <= 0): vx *= -1
+    if(y >= screen_height or y <= 0): vy *= -1
+    # pyautogui.alert(text='', title=pygetwindow.getAllTitles()[0],button='ok')
+    # print(pyautogui.position())
