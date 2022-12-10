70609c3ddda275adff4c3e7e58ea5641ed2e510f
Yunitrish <yunthomas006@gmail.com>
2022-12-10 18:24:15 +0800

    capture test

--- datas/leftclick/press/capture_demo.py
+++ datas/leftclick/press/capture_demo.py
@@ -1,11 +1,12 @@
 import cv2
 import pyautogui
+import os
+path = os.path.dirname(os.path.realpath(__file__))
+print(path)
 if __name__=='__main__':
     # 選擇第二隻攝影機
     cap = cv2.VideoCapture(1)
     i = 0
-    save_path = 'datas/test/'
-    file_name = 'test'
 
     while(True):
     # 從攝影機擷取一張影像
@@ -18,8 +19,8 @@ if __name__=='__main__':
         # 若按下 c 鍵則是拍照
         if cv2.waitKey(1) & 0xFF == ord('q'): break
         elif cv2.waitKey(1) & 0xFF == ord('c'): 
-            cv2.imwrite(save_path + file_name + '_' + str(i) + '.jpg',frame)
-            print('save:',file_name + '_' + str(i) + '.jpg')
+            cv2.imwrite(path + "\\" + str(i) + '.jpg',frame)
+            print('save:',path + "\\" + str(i) + '.jpg')
             i = i + 1
     # 釋放攝影機
     cap.release()
