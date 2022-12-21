4be6a1499b9723721a54f6063582855d43b7fa73
Hsuwan0715 <117660134+Hsuwan0715@users.noreply.github.com>
2022-12-21 10:57:35 +0800

    Add files via upload
    空氣滑鼠 左右滑動

---
+++ Left_Right_Move.py
@@ -0,0 +1,146 @@
+import cv2
+import mediapipe as mp
+import statistics
+import win32api, win32con
+import pyautogui
+import numpy as np
+from datetime import datetime
+mp_drawing = mp.solutions.drawing_utils
+mp_drawing_styles = mp.solutions.drawing_styles
+mp_hands = mp.solutions.hands
+
+#setting
+cd_leftmove = datetime.now().timestamp()
+cd_rightmove = datetime.now().timestamp()
+
+finger_center_temp = 0
+finger_center = 0
+
+sensitive = 4
+
+counter = 0
+        
+def moveMouse(var, direct, x):
+    if(varitation > 5):
+        x += var * sensitive * direct
+        win32api.SetCursorPos((round(x),round(y)))
+        
+
+# For static images:
+IMAGE_FILES = []
+with mp_hands.Hands(
+    static_image_mode=True,
+    max_num_hands=1,
+    min_detection_confidence=0.01) as hands:
+  for idx, file in enumerate(IMAGE_FILES):
+    # Read an image, flip it around y-axis for correct handedness output (see
+    # above).
+    image = cv2.flip(cv2.imread(file), 1)
+    # Convert the BGR image to RGB before processing.
+    results = hands.process(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+
+    # Print handedness and draw hand landmarks on the image.
+    print('Handedness:', results.multi_handedness)
+    if not results.multi_hand_landmarks:
+      continue
+    image_height, image_width, _ = image.shape
+    annotated_image = image.copy()
+    for hand_landmarks in results.multi_hand_landmarks:
+      print('hand_landmarks:', hand_landmarks)
+      print(
+          f'Index finger tip coordinates: (',
+          f'{hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].x * image_width}, '
+          f'{hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].y * image_height})'
+      )
+      mp_drawing.draw_landmarks(
+          annotated_image,
+          hand_landmarks,
+          mp_hands.HAND_CONNECTIONS,
+          mp_drawing_styles.get_default_hand_landmarks_style(),
+          mp_drawing_styles.get_default_hand_connections_style())
+    cv2.imwrite(
+        '/tmp/annotated_image' + str(idx) + '.png', cv2.flip(annotated_image, 1))
+    # Draw hand world landmarks.
+    if not results.multi_hand_world_landmarks:
+      continue
+    for hand_world_landmarks in results.multi_hand_world_landmarks:
+      mp_drawing.plot_landmarks(
+        hand_world_landmarks, mp_hands.HAND_CONNECTIONS, azimuth=5)
+
+# For webcam input:
+cap = cv2.VideoCapture(0,cv2.CAP_DSHOW)
+
+
+
+with mp_hands.Hands(
+    model_complexity=0,
+    min_detection_confidence=0.5,
+    min_tracking_confidence=0.5) as hands:
+  while cap.isOpened():
+    success, image = cap.read()
+    if not success:
+      print("Ignoring empty camera frame.")
+      # If loading a video, use 'break' instead of 'continue'.
+      continue
+
+    # To improve performance, optionally mark the image as not writeable to
+    # pass by reference.
+    image.flags.writeable = False
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    results = hands.process(image)
+
+    # Draw the hand annotations on the image.
+    image.flags.writeable = True
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+    w = image.shape[0]
+    h = image.shape[1]
+    
+    if results.multi_hand_landmarks:
+      for hand_landmarks in results.multi_hand_landmarks:
+        mp_drawing.draw_landmarks(
+            image,
+            hand_landmarks,
+            mp_hands.HAND_CONNECTIONS,
+            mp_drawing_styles.get_default_hand_landmarks_style(),
+            mp_drawing_styles.get_default_hand_connections_style())
+        
+        finger_tips = [5]
+        finger_tips.insert(0,hand_landmarks.landmark[4].x * w); #大拇指
+        finger_tips.insert(1,hand_landmarks.landmark[8].x * w);
+        finger_tips.insert(2,hand_landmarks.landmark[12].x * w);
+        finger_tips.insert(3,hand_landmarks.landmark[16].x * w);
+        finger_tips.insert(4,hand_landmarks.landmark[20].x * w); #小指
+        finger_center = statistics.mean(finger_tips);
+        if finger_center_temp == 0:
+            finger_center_temp = statistics.mean(finger_tips)
+        
+        varitation = abs(finger_center - finger_center_temp)
+        direction = np.sign(finger_center - finger_center_temp)*(-1)
+        
+        x = pyautogui.position()[0]
+        y = pyautogui.position()[1]
+        
+        moveMouse(varitation,direction,x)
+        #if(finger_center - finger_center_temp > 20):
+         #   print("Left!"+str(varitation))
+
+        #if(finger_center - finger_center_temp < -20):
+         #   print("Right!"+str(varitation))
+        #print(finger_center,finger_center_temp)
+        
+        
+        
+        
+        
+        finger_center_temp = statistics.mean(finger_tips); #計算平均數
+        
+        
+        
+        
+
+    # Flip the image horizontally for a selfie-view display.
+    cv2.imshow('MediaPipe Hands', cv2.flip(image, 1))
+    if cv2.waitKey(5) == ord('q'):
+      break
+cap.release()
+cv2.destroyAllWindows()
