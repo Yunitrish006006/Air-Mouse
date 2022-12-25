af342cb9611764f47cae70c488170b66e4759663
Hsuwan0715 <117660134+Hsuwan0715@users.noreply.github.com>
2022-12-25 20:48:36 +0800

    Update Main.py

--- Main.py
+++ Main.py
@@ -228,6 +228,33 @@ def move(hand_landmarks):
     finger_center_temp[0] = finger_center[0]
     finger_center_temp[1] = finger_center[1]
 #==========================================================================許家碩
+def moveCursor(var, move_type, direct, x, y): # call by Left_Right_move
+    sensitive = 4 # 靈敏度
+    if(var > 3):
+        if (move_type == "left_right"):
+            x += var * sensitive * direct
+            win32api.SetCursorPos((round(x),round(y)))
+
+def Left_Right_move(hand_landmarks): # call by hand_skeleton
+    finger_tips = [5]
+    finger_tips.insert(0,hand_landmarks.landmark[4].x * w) #大拇指
+    finger_tips.insert(1,hand_landmarks.landmark[8].x * w)
+    finger_tips.insert(2,hand_landmarks.landmark[12].x * w)
+    finger_tips.insert(3,hand_landmarks.landmark[16].x * w)
+    finger_tips.insert(4,hand_landmarks.landmark[20].x * w) #小指
+    finger_center = statistics.mean(finger_tips);  #計算平均數
+
+    if (finger_center_temp[0] == 0):
+        finger_center_temp[0] = finger_center
+
+    varitation = abs(finger_center - finger_center_temp[0])
+    direction = np.sign(finger_center - finger_center_temp[0])*(-1)
+
+    x = pyautogui.position()[0]
+    y = pyautogui.position()[1]
+    
+    moveCursor(varitation,"left_right",direction,x,y)
+    finger_center_temp[0] = finger_center
 
 #==========================================================================吳季旻
 def right(hand_landmarks):
